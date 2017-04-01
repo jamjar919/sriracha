7c0fa030447d467ab509b09a6bd41faed26a727a
jamjar919 <james@mjsquare.com>
2017-04-01 13:30:58 +0100

    passport auth

--- index.js
+++ index.js
@@ -2,14 +2,31 @@
 // load dependencies
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);;
+var http = require('http').Server(app);
+var passport = require('passport');
+var MonzoStrategy = require('passport-monzo').Strategy;
+var MonzoKeys = require('./monzosecret.json');
+const MONZO_CLIENT_ID = MonzoKeys.client_id;
+const MONZO_CLIENT_SECRET = MonzoKeys.client_secret;
 
 const port = process.env.PORT || 8080;
 
 app.get("/", function(req, res) {
-    res.send("hot hot heat");
+    res.send("bugger off");
 });
 
 http.listen(port, function(){
     console.log("Server up on http://localhost:%s", port);
 });
+
+passport.use(new MonzoStrategy({
+    clientID: MONZO_CLIENT_ID,
+    clientSecret: MONZO_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/monzo/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ monzoId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+))
