8c1025af42eb322ee4a7e3a77c7e5ebabb5c401f
jamjar919 <james@mjsquare.com>
2017-04-01 14:54:39 +0100

    get rid ofkey stuff

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-// Passport (For getting an auth token)
-// https://github.com/tombell/passport-monzo
-var passport = require('passport');
-var MonzoStrategy = require('passport-monzo').Strategy;
-
-// Sooper secret keys
-var MonzoKeys = require('./monzosecret.json');
-const MONZO_CLIENT_ID = MonzoKeys.client_id;
-const MONZO_CLIENT_SECRET = MonzoKeys.client_secret;
-
 // Development dev token
 var MonzoDevToken = require("./devtoken.json");
 var accessToken = MonzoDevToken.token;
@@ -81,25 +71,3 @@ app.get("/", function(req, res) {
 http.listen(port, function(){
     console.log("Server up on http://localhost:%s", port);
 });
-
-passport.use(new MonzoStrategy({
-        clientID: MONZO_CLIENT_ID,
-        clientSecret: MONZO_CLIENT_SECRET,
-        callbackURL: "http://google.com"
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        User.findOrCreate({ monzoId: profile.id }, function (err, user) {
-            return cb(err, user);
-        });
-    }
-))
-
-app.get('/auth/monzo', passport.authenticate('monzo'));
-
-app.get('/auth/monzo/callback', 
-    passport.authenticate('monzo', { failureRedirect: '/login' }),
-    function(req, res) {
-        // Successful authentication, redirect home.
-        res.redirect('/');
-    }
-);
