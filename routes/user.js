34e223e99f11d76b3e7052f9bf3b7bacc255ad9e
Thien Nguyen <t.nguyen@outlook.com>
2017-04-01 23:34:03 +0100

    added create budget api call

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
-
-module.exports = function(){
-	var express = require('express');
-	var app = express();
-        var db = require('./../database.js');
-	app.locals.basedir = "." + '/views';
-        var bodyParser = require('body-parser');
-        app.use(bodyParser.json()); // support json encoded bodies
-        app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+module.exports = function() {
+    var express = require('express');
+    var app = express();
+    var db = require('./../database.js');
+    app.locals.basedir = "." + '/views';
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json()); // support json encoded bodies
+    app.use(bodyParser.urlencoded({
+        extended: true
+    })); // support encoded bodies
 
     //The 404 Route (ALWAYS Keep this as the last route)
     app.get('/user/:user/', function(req, res) {
@@ -42,43 +43,47 @@ module.exports = function(){
                 .catch(function(error) {
                     res.send(error);
                 });
-            } else {
-                res.send(JSON.stringify({error:"Please provide parameters username, friendname, date, type and secret"}));
-            }
-	});
-        
-        app.post('/api/:user/addfriend/', function(req, res){
-            var username = req.params.user;
-            if (
-                (req.body.hasOwnProperty("name")) &&
-                (req.body.hasOwnProperty("phoneNumbers")) 
-            ) {
-                var friendname = req.body.name;
-                var phones = req.body.phoneNumbers;
-                db.addNewFriend(username, friendname, phones)
-                .then(function(data){
+        } else {
+            res.send(JSON.stringify({
+                error: "Please provide parameters username, friendname, date, type and secret"
+            }));
+        }
+    });
+
+    app.post('/api/:user/addfriend/', function(req, res) {
+        var username = req.params.user;
+        if (
+            (req.body.hasOwnProperty("name")) &&
+            (req.body.hasOwnProperty("phoneNumbers"))
+        ) {
+            var friendname = req.body.name;
+            var phones = req.body.phoneNumbers;
+            db.addNewFriend(username, friendname, phones)
+                .then(function(data) {
                     res.send(data);
                 })
                 .catch(function(error) {
                     res.send(error);
                 });
-            } else {
-                res.send(JSON.stringify({error:"Please provide parameters name and phoneNumbers"}));
-            }
-	});
-        
-    app.post('/api/adduser/', function(req, res){
-            console.log(req.body);
-            if (
-                (req.body.hasOwnProperty("username")) &&
-                (req.body.hasOwnProperty("name")) &&
-                (req.body.hasOwnProperty("monzoid"))
-            ) {
-                var username = req.body.username;
-                var realname = req.body.name;
-                var monzoid = req.body.monzoid;
-                db.addNewUser(username, realname, monzoid)
-                .then(function(data){
+        } else {
+            res.send(JSON.stringify({
+                error: "Please provide parameters name and phoneNumbers"
+            }));
+        }
+    });
+
+    app.post('/api/adduser/', function(req, res) {
+        console.log(req.body);
+        if (
+            (req.body.hasOwnProperty("username")) &&
+            (req.body.hasOwnProperty("name")) &&
+            (req.body.hasOwnProperty("monzoid"))
+        ) {
+            var username = req.body.username;
+            var realname = req.body.name;
+            var monzoid = req.body.monzoid;
+            db.addNewUser(username, realname, monzoid)
+                .then(function(data) {
                     res.send(data);
                 })
                 .catch(function(error) {
@@ -113,6 +118,26 @@ module.exports = function(){
             user: req.params.user
         }
         res.render('profile', parameters);
+    });
+
+    // api call to store users budget
+    app.post('/user/:user/budget', function(req, res) {
+        console.log(req.query);
+
+        var parameters = {
+            user: req.params.user
+            amount: req.params.amount
+            end: req.params.end
+        }
+
+        // store these in the database
+        db.getFriends(addNewBudget)
+            .then(function(data) {
+                res.send(JSON.stringify({
+                    "response": "ok",
+                    "message": data
+                }));
+            });
     });
 
     return app;
