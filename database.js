1b865dd1af447acbaacacbdeac55004ebebe343d
jamjar919 <james@mjsquare.com>
2017-04-01 17:54:44 +0100

    moved db functions into own space

---
+++ database.js
@@ -0,0 +1,63 @@
+var MongoClient = require('mongodb').MongoClient;
+var dbCreds = require("./database.json");
+const MONGO_USER = dbCreds.user;
+const MONGO_PASS = dbCreds.pass;
+const MONGO_URI = "mongodb://"+MONGO_USER+":"+MONGO_PASS+"@ds147510.mlab.com:47510/sriracha"
+MongoClient.connect(MONGO_URI, function(err, db) {
+    console.log("Connected successfully to remote database");
+    db.close();
+});
+
+module.exports.registerNewUser = function(username, realname) {
+    return new Promise(function(resolve,reject) {
+        MongoClient.connect(MONGO_URI, function(err, db) {
+            var collection = db.collection('users');
+            collection.insertOne({
+                username:username,
+                name:realname,
+                friends:[{name:"James Paterson",phone:"07908102754"}],
+                secrets:[{name:"James Paterson",secret:"he likes memes"}]
+            }, function(err,r) {
+                if (err == null) {
+                    resolve(r);
+                } else {
+                    reject(err,r);
+                }
+            });
+            db.close();
+        });
+    });
+}
+
+function isFriendInFriendList(friendname, friends) {
+    for (var i = 0; i < friends.length; i++) {
+        if (friendname == friends[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+module.exports.addNewSecret = function(username, friendname, secret) {
+    return new Promise(function(resolve,reject) {
+        MongoClient.connect(MONGO_URI, function(err, db) {
+            var user = db.collection('users').findOne({username:username})
+            if (isFriendInFriendList(friendname, user.friends)) {
+                // add the secret
+                var newSecrets = user.secrets;
+                newSecrets.push({name:friendname, secret:secret});
+                col.updateOne({username:username}, {$set: {secrets: newSecrets}}, {
+                        upsert: true
+                    },
+                    function(err, r) {
+                        if (err == null) {
+                            resolve(r);
+                        } else {
+                            reject(err,r);
+                        }
+                    }
+                );
+            }
+        });
+    });
+}
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const pug = require('pug');
 var app = express();
 const server = require('http').Server(app);
 var request = require('request');
+var db = require('./database.js');
 
 // manage views
 app.set('views', __dirname + '/views')
@@ -17,17 +18,6 @@ var MonzoKeys = require('./monzosecret.json');
 const MONZO_CLIENT_ID = MonzoKeys.client_id;
 const MONZO_CLIENT_SECRET = MonzoKeys.client_secret;
 
-// Mongo
-var MongoClient = require('mongodb').MongoClient;
-var dbCreds = require("./database.json");
-const MONGO_USER = dbCreds.user;
-const MONGO_PASS = dbCreds.pass;
-const MONGO_URI = "mongodb://"+MONGO_USER+":"+MONGO_PASS+"@ds147510.mlab.com:47510/sriracha"
-MongoClient.connect(MONGO_URI, function(err, db) {
-    console.log("Connected successfully to remote database");
-    db.close();
-});
-
 // Development dev token
 var MonzoDevToken = require("./devtoken.json");
 var accessToken = MonzoDevToken.token;
@@ -39,59 +29,6 @@ var monzo = require('monzo-bank');
 
 const port = process.env.PORT || 8080;
 
-function registerNewUser(username, realname) {
-    return new Promise(function(resolve,reject) {
-        MongoClient.connect(MONGO_URI, function(err, db) {
-            var collection = db.collection('users');
-            collection.insertOne({
-                username:username,
-                name:realname,
-                friends:[{name:"James Paterson",phone:"07908102754"}],
-                secrets:[{name:"James Paterson",secret:"he likes memes"}]
-            }, function(err,r) {
-                if (err == null) {
-                    resolve(r);
-                } else {
-                    reject(err,r);
-                }
-            });
-            db.close();
-        });
-    });
-}
-
-function isFriendInFriendList(friendname, friends) {
-    for (var i = 0; i < friends.length; i++) {
-        if (friendname == friends[i]) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function addNewSecret(username, friendname, secret) {
-    return new Promise(function(resolve,reject) {
-        MongoClient.connect(MONGO_URI, function(err, db) {
-            var user = db.collection('users').findOne({username:username})
-            if (isFriendInFriendList(friendname, user.friends)) {
-                // add the secret
-                var newSecrets = user.secrets;
-                newSecrets.push({name:friendname, secret:secret});
-                col.updateOne({username:username}, {$set: {secrets: newSecrets}}, {
-                        upsert: true
-                    },
-                    function(err, r) {
-                        if (err == null) {
-                            resolve(r);
-                        } else {
-                            reject(err,r);
-                        }
-                    }
-                );
-            }
-        });
-    });
-}
 
 function isValidToken(token) {
     return new Promise(function(resolve, reject) {
