8375911460b61c136b38d4099b252fb17453e554
jamjar919 <james@mjsquare.com>
2017-04-01 13:35:24 +0100

    diagd

--- index.js
+++ index.js
@@ -20,13 +20,23 @@ http.listen(port, function(){
 });
 
 passport.use(new MonzoStrategy({
-    clientID: MONZO_CLIENT_ID,
-    clientSecret: MONZO_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/monzo/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ monzoId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+        clientID: MONZO_CLIENT_ID,
+        clientSecret: MONZO_CLIENT_SECRET,
+        callbackURL: "http://127.0.0.1:3000/auth/monzo/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ monzoId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
 ))
+
+app.get('/auth/monzo', passport.authenticate('monzo'));
+
+app.get('/auth/monzo/callback', 
+    passport.authenticate('monzo', { failureRedirect: '/login' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    }
+);
