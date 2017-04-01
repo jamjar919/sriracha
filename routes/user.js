778406203f0c0bc7a93228ad97386904b8f3d1f6
Thien Nguyen <t.nguyen@outlook.com>
2017-04-01 17:08:35 +0100

    added pug and bower files

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,16 @@
 module.exports = function(){
 	var express = require('express');
 	var app = express();
-	// app.locals.basedir = "." + '/views';
+	app.locals.basedir = "." + '/views';
 
 	//The 404 Route (ALWAYS Keep this as the last route)
-	app.get('/:user/', function(req, res){
+	app.get('/user/:user/', function(req, res){
     console.log(req.params);
-		var userid = req.params.user;
+		// var userid = req.params.user;
+		var parameters = {
+			user : req.params.user
+		}
+		res.render('profile', parameters);
 	});
 
 	app.get('/:user/api/', function(req, res){
