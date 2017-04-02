950465dc14a7546e5b45233b0786068ac32ee14e
Thien Nguyen <t.nguyen@outlook.com>
2017-04-02 01:46:32 +0100

    initial commit for webhooks

--- index.js
+++ index.js
@@ -34,11 +34,72 @@ app.use(express.static(__dirname + '/public'));
 // users stuff
 app.use('/', require('./routes/user'));
 
-// monzo api
-app.use('/', require('./routes/monzo'));
-
 // webhook
 app.use('/', require('./routes/webhook'));
+
+
+
+var monzo = require('monzo-bank');
+var accessToken;
+var accountId;
+try {
+		var MonzoDevToken = require("../devtoken.json");
+		accessToken = MonzoDevToken.token;
+		accountId = MonzoDevToken.account_id;
+		console.log("got james keys from json")
+} catch (e) {
+		console.log("got james keys from heroku")
+		accessToken = process.env.MONZO_JAMES_TOKEN;
+		accountId = process.env.MONZO_JAMES_ID;
+}
+
+var baseurl;
+if (process.env.NODE && ~process.env.NODE.indexOf("heroku")) {
+		baseurl = "https://sriracha-app.herokuapp.com:" + String(port);
+		console.log("server is running on heroku");
+} else {
+		baseurl = "http://localhost:" + String(port);
+		console.log("server is running locally");
+}
+
+// // monzo api
+app.get("/monzo-connect", function(req, res) {
+    console.log("going through monzo connect")
+    if (req.query.hasOwnProperty("code")) {
+      console.log("its actually going");
+        var code = req.query.code;
+        request.post('https://api.monzo.com/oauth2/token', {
+            form: {
+                grant_type: "authorization_code",
+                client_id: MONZO_CLIENT_ID,
+                client_secret: MONZO_CLIENT_SECRET,
+                redirect_uri: baseurl + "/monzo-connect",
+                code: code
+            }
+        }, function(error, response, body) {
+            body = JSON.parse(body);
+            if (body.hasOwnProperty("access_token")) {
+                var username = Math.random().toString(36).slice(2);
+                var name = "Default Name";
+                var monzoid = body.user_id;
+                db.addNewUser(username, realname, monzoid)
+                .then(function(data) {
+                    res.redirect("helpmebudget://monzo-connect?access_token=" + response.access_token + "&refresh_token=" + response.refresh_token + "&username=" + username + "&user_id="+monzoid);
+                })
+                .catch(function(error) {
+                    res.send(error);
+                });
+            } else {
+                res.send(body);
+            }
+        });
+    } else {
+      console.log("sorry pal");
+        res.send(JSON.stringify({
+            error: "No code supplied"
+        }));
+    }
+});
 
 
 server.listen(port, function(){
--- routes/webhook.js
+++ routes/webhook.js
@@ -2,10 +2,14 @@ module.exports = function(){
     var express = require('express');
     var app = express();
     var db = require('./../database.js');
-    
-    app.get('/webhook', function(req, res){
-        
+    app.locals.basedir = "." + '/views';
+
+    app.post('/webhook', function(req, res){
+
+      console.log(req.body);
+      res.status(200);
+      res.send();
     });
-    
+
     return app;
 }();
