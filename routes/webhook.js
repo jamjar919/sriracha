0a044818dc7d717f1b317d6a1192d0ebf07cd03c
Thien Nguyen <t.nguyen@outlook.com>
2017-04-02 02:07:40 +0100

    update webhooks

--- routes/webhook.js
+++ routes/webhook.js
@@ -7,9 +7,41 @@ module.exports = function(){
     app.post('/webhook', function(req, res){
 
       console.log(req.body);
+
+      if (req.body.type === "transaction.created"){
+        // we know it's a transaction, lets see what it does.
+        var data = req.body.data;
+        var date_updated = data.updated;
+        var persons_id = data.account_id;
+        var amount = data.amount;
+        manageTransaction()
+      }
+
+      // send nice response back to monzo
       res.status(200);
       res.send();
     });
 
     return app;
 }();
+
+function getUsername(person_id){
+
+  return username;
+}
+
+function getBudget(username){
+
+}
+
+function manageTransaction(){
+
+}
+
+function checkBudgetIsActive(){
+
+}
+
+function updateBudgetValue(user_id, amount){
+
+}
