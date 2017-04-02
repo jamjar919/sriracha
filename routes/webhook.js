788c29cc0f72dc341ec3c9d4b8ba92bfbf8f203d
Thien Nguyen <t.nguyen@outlook.com>
2017-04-02 02:26:52 +0100

    updated webhook commands

--- routes/webhook.js
+++ routes/webhook.js
@@ -11,12 +11,40 @@ module.exports = function(){
       if (req.body.type === "transaction.created"){
         // we know it's a transaction, lets see what it does.
         var data = req.body.data;
-        var date_updated = data.updated;
+        var date_updated = new Date(data.updated);
         var persons_id = data.account_id;
         var amount = data.amount;
-        manageTransaction()
-      }
 
+        // get the username
+        db.getUsername(persons_id)
+          .then(function(username){
+            // then get the username's budget value
+            db.getBudget(username)
+              .then(function(result){
+                // check the date first
+                if (result.time < date_updated){
+                  // then check if the budget is ok after adding
+                  if (result.value += amount > result.value){
+                    // if it is:
+                      // expose secrets
+                      db.exposeNewSecret(username)
+                      // send twilio stuff
+                      // reset.
+                  } else {
+                    // increment it
+                  }
+                } else {
+                  // we've surpassed the date.
+                }
+              })
+              .catch(function(error){
+                // we don't have a budget for this person!
+              });
+          })
+          .catch(function(error){
+            // we can't find this username.
+          })
+      }
       // send nice response back to monzo
       res.status(200);
       res.send();
@@ -31,7 +59,10 @@ function getUsername(person_id){
 }
 
 function getBudget(username){
+  db.getBudget(username)
+  .then(function(data) {
 
+  })
 }
 
 function manageTransaction(){
